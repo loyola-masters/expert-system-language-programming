09fc51d8e8cf111a3c6f30a38c683341dd78eca2
Bernardo Ronquillo <bernardo.ronquillo@myhixel.com>
2023-06-11 19:35:32 +0200

    Courses knowledge base

---
+++ course_recommender/courses_kb.pl
@@ -0,0 +1,201 @@
+% Knowledge base of courses for:
+% php, ruby, csharp, objectivec, c (none), javascript (many), java (many), python (many)
+
+% php
+course(building_a_dynamic_web_app_using_php_mysql,php) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_php(databases).
+
+course(building_web_applications_in_php,php) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_php(web_application).
+
+
+% ruby
+course(ruby_on_rails_an_introduction,ruby) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_ruby(github).
+
+
+% csharp
+course(control_physics_with_csharp_in_unity,csharp) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_csharp(computer_programming).
+
+course(create_power-ups_and_obstacles_with_csharp_in_unity,csharp) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_csharp(software_engineering).
+
+course(manipulate_object_properties_with_csharp_in_unity,csharp) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_csharp(object_z).
+
+course(csharp_programming_for_unity_game_development_capstone_project,csharp) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_csharp(documents).
+
+course(learn_to_code_using_csharp_on_vs_code,csharp) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_csharp(data_type).
+
+course(create_keyboard_movement_mechanics_with_csharp_in_unity,csharp) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_csharp(video_game_development).
+
+course(create_basic_behavior_with_csharp_in_unity,csharp) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_csharp(block_code).
+
+
+% objectivec
+course(foundations_of_objective_c_app_development,objectivec) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_objetivec(ios_development).
+
+% c
+
+% javascript
+course(javascript_numbers_properties_and_methods,javascript) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_javascript(ajax).
+
+course(javascript_animation_for_websites_storytelling_data_visualization_and_games,javascript) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_javascript(html).
+
+course(web_application_development_with_javascript_and_mongodb,javascript) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_javascript(mongodb).
+
+course(hosting_a_static_website_html_css_javascript_in_aws_s3,javascript) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_javascript(web_hosting_service).
+
+course(front_end_javascript_frameworks_angular,javascript) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_javascript(front_end_js).
+
+course(responsive_website_basics_code_with_html_css_and_javascript,javascript) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_javascript(jquery).
+
+course(learn_javascript_with_zero_prior_programming_experience,javascript) :-
+	nivel(advanced),
+	min_rate(medium),
+	skills_javascript(recursive_function).
+
+course(javascript_jquery_and_json,javascript) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_javascript(django).
+
+course(interactivity_with_javascript,javascript) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_javascript(document_object_model).
+
+course(become_a_javascript_pro_with_these_7_skills,javascript) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_javascript(web_application).
+
+course(quick_resumecreator_with_javascript,javascript) :-
+	nivel(advanced),
+	min_rate(medium),
+	skills_javascript(web_development).
+
+% java
+course(java_classes_and_objects,java) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_java(eulers_totient_function).
+
+%.  --- This is about Javascript ---
+course(web_application_development_with_javascript_and_mongodb,java) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_java(meteor).
+
+course(learn_to_teach_java_sequences_primitive_types_and_using_objects,java) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_java(code_segment).
+
+course(learn_javascript_with_zero_prior_programming_experience,java) :-
+	nivel(advanced),
+	min_rate(medium),
+	skills_java(recursive_function).
+
+course(create_your_first_application_with_java_using_networking,java) :-
+	nivel(beginner),
+	min_rate(medium),
+	skills_java(project).
+
+course(building_scalable_java_microservices_with_spring_boot_and_spring_cloud,java) :-
+	nivel(beginner),
+	min_rate(medium),
+	skills_java(build_operate_transfer).
+
+course(advanced_data_structures_in_java,java) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_java(dijkstras_algorithm).
+
+course(java_for_android,java) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_java(java_syntax).
+
+
+% python 
+course(intermediate_pandas_python_library_for_data_science,python) :-
+	nivel(beginner),
+	min_rate(high),
+	skills_python(pandas).
+
+course(create_your_own_sudoku_solver_using_ai_and_python,python) :-
+	nivel(beginner),
+	min_rate(medium),
+	skills_python(algorithms).
+
+course(advanced_portfolio_construction_and_analysis_with_python,python) :-
+	nivel(advanced),
+	min_rate(high),
+	skills_python(measurement).
+
+course(applied_text_mining_in_python,python) :-
+	nivel(advanced),
+	min_rate(medium),
+	skills_python(natural_language).
+
+course(python_and_machine_learning_for_asset_management,python) :-
+	nivel(advanced),
+	min_rate(low),
+	skills_python(cross_validation).
+
+course(computer_vision_object_detection_with_opencv_and_python,python) :-
+	nivel(beginner),
+	min_rate(medium),
+	skills_python(object_detection).
+
+course(version_control_of_a_python_project_using_git,python) :-
+	nivel(intermediate),
+	min_rate(high),
+	skills_python(python_software).
