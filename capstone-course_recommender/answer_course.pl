0974d3bd1537297987c315555320f2824c2a964f
Bernardo Ronquillo <bernardo.ronquillo@myhixel.com>
2023-06-12 13:14:18 +0200

    Fix capstone files

--- capstone-course_recommender/answer_course.pl
+++ capstone-course_recommender/answer_course.pl
@@ -23,3 +23,171 @@ answer(yes) :-
   
 answer(no) :-
   write('no').
+
+answer(rice_university) :-
+  write('Rice University').
+  
+answer(coursera_project_network) :-
+  write('Coursera Project Network').
+
+answer(vanderbilt_university) :-
+  write('Vanderbilt University').
+  
+answer(university_of_california_san_diego) :-
+  write('University of California San Diego').
+  
+answer(johns_hopkins_university) :-
+  write('Johns Hopkins University').
+  
+answer(university_of_california_irvine) :-
+  write('University of California Irvine').
+  
+answer(beginner) :-
+  write('Im a begginer').
+  
+answer(intermediate) :-
+  write('Im intermediate').
+  
+answer(advanced) :-
+  write('Im advanced').
+  
+answer(python_programming) :-
+  write('python programming').
+  
+answer(computer_programming) :-
+  write('computer programming').  
+  
+answer(computer_science) :-
+  write('computer science').  
+  
+answer(software_development) :-
+  write('software development').  
+  
+answer(programming_style) :-
+  write('programming style').  
+  
+answer(operations_management) :-
+  write('operations management').  
+  
+answer(approximation_error) :-
+  write('approximation error').  
+  
+answer(syntax) :-
+  write('syntax').  
+  
+answer(sql) :-
+  write('sql').  
+  
+answer(object_oriented_programming) :-
+  write('object oriented programming').  
+  
+answer(mysql) :-
+  write('mysql').  
+  
+answer(pandas) :-
+  write('pandas').  
+  
+answer(applied_machine_learning) :-
+  write('applied machine learning').  
+  
+answer(data_science) :-
+  write('data science').  
+  
+answer(numpy) :-
+  write('numpy').  
+  
+answer(data_analysis) :-
+  write('data analysis').  
+  
+answer(mobile_app) :-
+  write('mobile app').  
+  
+answer(internet_of_things) :-
+  write('internet of things').  
+  
+answer(front_end) :-
+  write('front end').  
+  
+answer(back_end) :-
+  write('back end').  
+  
+answer(concatenation) :-
+  write('concatenation').  
+  
+answer(holding_value) :-
+  write('holding value').  
+  
+answer(text_corpus) :-
+  write('text corpus').  
+  
+answer(hypertext_transfer_protocol) :-
+  write('hypertext transfer protocol').  
+  
+answer(web_api) :-
+  write('web api').  
+  
+answer(software_development) :-
+  write('software development').  
+  
+answer(data_visualization) :-
+  write('data visualization').  
+  
+answer(infographics) :-
+  write('infographics').  
+  
+answer(web_development) :-
+  write('web development').  
+  
+answer(calibration) :-
+  write('calibration').  
+  
+answer(java_platform) :-
+  write('java platform').  
+  
+answer(android_development) :-
+  write('android development').  
+  
+answer(graph_algorithms) :-
+  write('graph algorithms').  
+  
+answer(graph_theory) :-
+  write('graph theory').  
+  
+answer(search_algorithm) :-
+  write('search algorithm').  
+  
+answer(sorting) :-
+  write('sorting').  
+  
+answer(evaluation) :-
+  write('evaluation').  
+  
+answer(education) :-
+  write('education').  
+  
+answer(github) :-
+  write('github').  
+  
+answer(rubygems) :-
+  write('rubygems').  
+  
+answer(databases) :-
+  write('databases').  
+  
+answer(web_page) :-
+  write('web page').  
+  
+answer(mobile_development) :-
+  write('mobile development').  
+  
+answer(iOS_development) :-
+  write('iOS development').  
+  
+answer(swift_programming) :-
+  write('swift programming').  
+
+answer(high) :-
+  write('high').  
+    
+answer(low) :-
+  write('low').  
